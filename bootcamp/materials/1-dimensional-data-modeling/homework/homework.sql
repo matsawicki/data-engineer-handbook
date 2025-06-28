e2854512c21cd2cde7416519d22a4d60d414150a
Mateusz Sawicki <mateusz@geew.dev>
2025-06-29 00:02:49 +0200

    finished task 1. todo: implement while loop to populate table

--- bootcamp/materials/1-dimensional-data-modeling/homework/homework.sql
+++ bootcamp/materials/1-dimensional-data-modeling/homework/homework.sql
@@ -3,6 +3,7 @@
 drop type if exists films cascade;
 
 create type films as (
+	"year" int4,
 	filmid text,
 	film text,
 	votes int4,
@@ -19,10 +20,10 @@ create type quality_class as enum ('star',
 drop table if exists actors;
 
 create table actors (
-	actorid text,
+	actorid text ,
 	actor text,
 	current_year int4,
-	films films,
+	films films[],
 	quality_class quality_class,
 	is_active bool,
 	primary key (actorid,
@@ -30,31 +31,47 @@ current_year)
 
 );
 
-insert into public.actors 
+insert
+	into
+	public.actors 
 with previous_year as (
-select
-	*
-from
-	public.actors
-where
-	current_year = 1969
+	select
+		*
+	from
+		public.actors
+	where
+		current_year = 1971
 ),
 
-current_year as (
-select
-	actorid,
-	actor,
-	"year",
-	array_agg(row(filmid, film, votes, rating)::films) as films, 
-	avg(rating) as average_rating
-from
-	public.actor_films
-where
-	"year" = 1970
-group by
-	actorid,
-	actor,
-	"year"
+
+	current_year as (
+	select
+		actorid,
+		actor,
+		"year",
+		array_agg(row("year", filmid, film, votes, rating)::films) as films,
+		avg(rating) as average_rating
+	from
+		public.actor_films
+	where
+		"year" = 1972
+	group by
+		actorid,
+		actor,
+		"year"
+		),
+	current_year_with_quality_class as (
+	select
+		* ,
+		case
+			when average_rating <= 6 then 'bad'
+			when average_rating <= 7 then 'average'
+			when average_rating <= 8 then 'good'
+			else 'star'
+		end as quality_class
+	from
+		current_year
+
 )
 
 
@@ -67,16 +84,13 @@ select
 			ct.films
 		else py.films || ct.films
 	end as films,
+	ct.quality_class::quality_class as quality_class,
 	case
-		when ct.average_rating <= 6 then 'bad'
-		when ct.average_rating <= 7 then 'average'
-		when ct.average_rating <= 8 then 'good'
-		else 'star'
-	end as quality_class,
-	1 as is_active
-	-- todo
+		when ct.films is not null then true
+		else false
+	end as is_active
 from
-		current_year as ct
+		current_year_with_quality_class as ct
 full join previous_year as py
 	on
 		ct.actorid = py.actorid;
